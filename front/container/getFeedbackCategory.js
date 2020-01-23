f3029cd74d9e794d503e826889c75c506fd2f06c
SangchoKim <wjdrms1919@gmail.com>
2020-01-24 00:37:17 +0900

    FIX:err

---
+++ front/container/getFeedbackCategory.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+import Link from 'next/link';
+import { Drawer, Card, Input, Icon, Button, Col, Typography, Row, Avatar } from 'antd';
+
+const {Title} = Typography;
+const { Meta } = Card;
+
+const getFeedbackCategory = ({reqFeedbackCategoryVisble,reqFeedbackCategoryHandleCancel}) => {
+
+    return(
+        <>
+            <Drawer
+                key='alert'
+                title={<strong>피드백 요청 목록</strong>}
+                placement="right"
+                visible={reqFeedbackCategoryVisble}
+                style={{padding:0, }}
+                onClose={reqFeedbackCategoryHandleCancel}
+            >
+            </Drawer>
+        </>
+    )
+};
+
+export default getFeedbackCategory;
